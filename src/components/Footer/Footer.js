2e475e4d7990c6d6d0bc50f89e68c94cbabd136d
adityabhagat007 <rajarambhagat6582@gmail.com>
2022-12-25 03:19:37 +0530

    ui update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,51 @@
-import React from "react";
-// import styles from './Footer.module.css';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 
-function Footer() {
-  return <div>Footer</div>;
+function Copyright() {
+  return (
+    <Typography variant="body2" style={{  background: '#000000',
+    color:'#ffff'}}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        rescuetag
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Footer;
+export default function StickyFooter() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      
+      }}
+    >
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto', 
+          background: '#000000',
+          color:'#ffff'
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            Sticky footer
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
+  );
+}
--- src/pages/signUpPage.js
+++ src/pages/signUpPage.js
@@ -1,15 +1,24 @@
-import React from "react"
+import { Container, Grid } from "@mui/material";
+import React from "react";
 import Layout from "../layout/layout";
 
-const SignUpPage = ()=>{
+const SignUpPage = () => {
   return (
     <>
-    <Layout>
-        <h1>Register</h1>
-    </Layout>
+      <Layout>
+        <Container maxWidth="lg">
+          <Grid>
+            <Grid item xs={12} lg={2} md={6}>
+              item1
+            </Grid>
+            <Grid item xs={12} lg={2} md={6}>
+              item2
+            </Grid>
+          </Grid>
+        </Container>
+      </Layout>
     </>
-  )
-}
-
+  );
+};
 
 export default SignUpPage;
