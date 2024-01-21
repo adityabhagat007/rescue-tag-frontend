4f024d7d903e20b32f0c357ec58b28de86248fa7
adityabhagat007 <rajarambhagat6582@gmail.com>
2024-01-21 13:04:02 +0530

    local route setup

---
+++ src/AppContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+export default function AppContainer() {
+  return (
+        <App />
+  );
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,24 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeLayout from "../layout/homeLayout";
+import AboutUsPage from "../pages/aboutUsPage";
 import HomePage from "../pages/homePage";
-import Page404 from "../pages/page404";
-import AboutPage from "../pages/aboutUsPage";
 import LoginPage from "../pages/loginPage";
 import SignUpPage from "../pages/signUpPage";
 
-const AllRoutes = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<SignUpPage />} />
-          <Route path="/*" element={<Page404 />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-};
 
-export default AllRoutes;
+
+export const publicRoutes = [
+  {
+    path:"/",
+    element:<HomeLayout/>,
+    children:[
+      {
+        path:"",
+        element:<HomePage/>
+      },
+      {
+        path:"/about",
+        element:<AboutUsPage/>
+      },
+      {
+        path:"/login",
+        element:<LoginPage/>
+      },
+      {
+        path:"/signup",
+        element:<SignUpPage/>
+      }
+    ]
+  }
+]
+
+export const privateRoute = [
+]
